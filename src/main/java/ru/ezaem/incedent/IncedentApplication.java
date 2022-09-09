292f318a68626f79d8b2d8e1c6e2ef9143b335e0
Ilya Shatalov <ilya@alov.me>
2022-09-09 19:33:56 +0300

    init commit

---
+++ src/main/java/ru/ezaem/incedent/IncedentApplication.java
@@ -0,0 +1,13 @@
+package ru.ezaem.incedent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncedentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncedentApplication.class, args);
+	}
+
+}
